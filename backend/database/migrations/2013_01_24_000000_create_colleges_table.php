dc9a46533de84ecfafa8e511edf33858fe0fda31
shramana263 <shramana652@gmail.com>
2025-01-30 01:59:43 +0530

    Add address and contact number fields to colleges table migration

--- backend/database/migrations/2013_01_24_000000_create_colleges_table.php
+++ backend/database/migrations/2013_01_24_000000_create_colleges_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('code')->nullable();
+            $table->string('address')->nullable();
+            $table->string('contact_no')->nullable();
         });
     }
 
